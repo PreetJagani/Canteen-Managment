8f5d05ae44e32e8ffa81480170023d1ec73aa6d3
PreetJagani <preetjagani@gmail.com>
2020-10-06 15:02:32 +0530

    cart item store functionality added

--- app/src/main/java/com/canteenmanagment/helper/AddCartCustomDiolog.kt
+++ app/src/main/java/com/canteenmanagment/helper/AddCartCustomDiolog.kt
@@ -3,40 +3,59 @@ package com.canteenmanagment.helper
 import android.app.Activity
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Context
 import android.view.LayoutInflater
 import com.canteenmanagment.R
+import com.canteenmanagment.canteen_managment_library.models.CartFood
 import com.canteenmanagment.canteen_managment_library.models.Food
+import com.canteenmanagment.ui.FoodList.FoodListActivity
 import com.daimajia.androidanimations.library.Techniques
 import com.daimajia.androidanimations.library.YoYo
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.add_cart_custome_diolog.view.*
+import java.lang.reflect.Type
 
-class AddCartCustomDiolog(val activity : Activity) {
+class AddCartCustomDiolog(val activity: Activity) {
 
-    lateinit var alertDialog : Dialog
+    lateinit var alertDialog: Dialog
     private val MAX_ITEM = 5
     private val MIN_ITEM = 0
+    val prefrence = activity.getSharedPreferences(FoodListActivity.CART, 0x0000)
 
-    fun startDialog(food : Food){
+    var gson = Gson()
+    val type: Type = object : TypeToken<MutableList<CartFood>?>() {}.type
+
+    fun startDialog(food: Food, mContext: Context) {
 
         var dialog = AlertDialog.Builder(activity)
 
-        var view = LayoutInflater.from(activity).inflate(R.layout.add_cart_custome_diolog,null)
+        var view = LayoutInflater.from(activity).inflate(R.layout.add_cart_custome_diolog, null)
 
         view.IM_plus.setOnClickListener {
-            if(view.TV_item_count.text.toString().toInt() < MAX_ITEM){
+
+            if (view.TV_item_count.text.toString().toInt() < MAX_ITEM) {
                 YoYo.with(Techniques.BounceInUp).duration(500).playOn(view.TV_item_count)
-                view.TV_item_count.text = (view.TV_item_count.text.toString().toInt() + 1).toString()
+                view.TV_item_count.text =
+                    (view.TV_item_count.text.toString().toInt() + 1).toString()
+
+                updateCart(view.TV_item_count.text.toString().toInt(), food)
             }
 
         }
+
         view.IM_minus.setOnClickListener {
-            if(view.TV_item_count.text.toString().toInt() > MIN_ITEM){
+            if (view.TV_item_count.text.toString().toInt() > MIN_ITEM) {
                 YoYo.with(Techniques.BounceInDown).duration(500).playOn(view.TV_item_count)
-                view.TV_item_count.text = (view.TV_item_count.text.toString().toInt() -1).toString()
+                view.TV_item_count.text =
+                    (view.TV_item_count.text.toString().toInt() - 1).toString()
+
+                updateCart(view.TV_item_count.text.toString().toInt(), food)
             }
         }
+
         view.TV_name.text = food.name
-        view.TV_price.text ="(${food.price}  Rs.)"
+        view.TV_price.text = "(${food.price}  Rs.)"
 
         dialog.setView(view)
 
@@ -46,10 +65,53 @@ class AddCartCustomDiolog(val activity : Activity) {
         alertDialog.getWindow()?.setBackgroundDrawableResource(android.R.color.transparent)
 
 
+        var cartItemString = prefrence.getString(FoodListActivity.CART_ITEMS, null)
 
+        var cartItemLIst = if (cartItemString != null)
+            gson.fromJson<List<CartFood>>(cartItemString, type)
+        else
+            emptyList()
+
+        for (cartItem in cartItemLIst)
+            if (cartItem.food.id.equals(food.id)) {
+                view.TV_item_count.text = cartItem.qunity.toString()
+                break;
+            }
     }
 
-    fun stopDiaolog(){
+    fun stopDiaolog() {
         alertDialog.dismiss()
     }
+
+    private fun updateCart(quantity: Int, food: Food) {
+
+        var cartItemString = prefrence.getString(FoodListActivity.CART_ITEMS, null)
+
+        var cartItemList = if (cartItemString != null)
+            gson.fromJson<MutableList<CartFood>>(cartItemString, type)
+        else
+            mutableListOf()
+
+        var flag = false
+        for (cartItem in cartItemList)
+            if (cartItem.food.id.equals(food.id)) {
+                cartItemList.remove(cartItem)
+                if (quantity != 0)
+                    cartItemList.add(CartFood(quantity, food))
+                flag = true
+                break
+            }
+
+        if (!flag)
+            cartItemList.add(CartFood(quantity, food))
+
+        val editor = prefrence.edit()
+
+        var cartListString = gson.toJson(cartItemList)
+        editor.putString(FoodListActivity.CART_ITEMS, cartListString)
+
+        editor.apply()
+
+    }
+
 }
--- app/src/main/java/com/canteenmanagment/ui/FoodList/FoodListActivity.kt
+++ app/src/main/java/com/canteenmanagment/ui/FoodList/FoodListActivity.kt
@@ -2,18 +2,21 @@ package com.canteenmanagment.ui.FoodList
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.canteenmanagment.BaseActivity.BaseActivity
 import com.canteenmanagment.Fragments.MenuFragment.Companion.CATEGORY_NAME
 import com.canteenmanagment.R
 import com.canteenmanagment.canteen_managment_library.apiManager.FirebaseApiManager
+import com.canteenmanagment.canteen_managment_library.models.CartFood
 import com.canteenmanagment.canteen_managment_library.models.Food
 import com.canteenmanagment.databinding.ActivityFoodListBinding
-import com.canteenmanagment.databinding.AddCartCustomeDiologBinding
-import kotlinx.coroutines.launch
 import com.canteenmanagment.helper.AddCartCustomDiolog
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.launch
+import java.lang.reflect.Type
 
 class FoodListActivity : BaseActivity(), View.OnClickListener {
 
@@ -28,7 +31,8 @@ class FoodListActivity : BaseActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_food_list)
 
-        binding = DataBindingUtil.setContentView(this,
+        binding = DataBindingUtil.setContentView(
+            this,
             R.layout.activity_food_list
         )
         binding.title = intent.getStringExtra(CATEGORY_NAME)
@@ -45,6 +49,18 @@ class FoodListActivity : BaseActivity(), View.OnClickListener {
         }
 
 
+        val prefrence = mContext.getSharedPreferences(CART, 0x0000)
+        var cartItemString = prefrence.getString(FoodListActivity.CART_ITEMS, null)
+        var gson = Gson()
+        val type: Type = object : TypeToken<MutableList<CartFood>?>() {}.type
+
+        var cartItemList = if (cartItemString != null)
+            gson.fromJson<MutableList<CartFood>>(cartItemString, type)
+        else
+            mutableListOf()
+
+        Log.d("Cart",cartItemList.toString())
+
     }
 
     private fun loadData() {
@@ -55,8 +71,8 @@ class FoodListActivity : BaseActivity(), View.OnClickListener {
                 binding.RVFoodList.visibility = View.VISIBLE
                 binding.RVFoodList.adapter = FoodListRecyclerViewAdapter(it,
                     FoodListRecyclerViewAdapter.ClickListner { position ->
-                        if(foodList[position].available)
-                            addCartCustomDiolog.startDialog(foodList.get(position))
+                        if (foodList[position].available)
+                            addCartCustomDiolog.startDialog(foodList.get(position),mContext)
                     }
                 )
             }
@@ -76,6 +92,13 @@ class FoodListActivity : BaseActivity(), View.OnClickListener {
     override fun onBackPressed() {
         binding.CL.visibility = View.INVISIBLE
         super.onBackPressed()
+
+    }
+
+    companion object{
+
+        const val CART = "Cart"
+        const val CART_ITEMS = "Cart Items"
 
     }
 
